5efa49849d9bec27fb0d3d793c2b4c0872c55ee8
Claudia Villarroel <claudia.villarroel@live.com>
2024-01-02 21:32:04 -0300

    :hammer: Eventos en JS

--- script.js
+++ script.js
@@ -1,37 +1,11 @@
 const h1 = document.querySelector("h1");
-const p = document.querySelector("p");
-const parrafito = document.querySelector(".parrafito");
-const pid = document.querySelector("#pid");
-const input = document.querySelector("input");
+const input1 = document.querySelector("#calculo1");
+const input2 = document.querySelector("#calculo2");
+const btn = document.querySelector("#btnCalcular");
+let result = document.querySelector("#result");
 
-console.log(input.value);
-
-
-console.log({
-    h1,
-    p,
-    parrafito,
-    pid,
-    input
-});
-
-h1.innerHTML="Patito <br> Feo";
-h1.innerText="Patito <br> Feo";
-/*
-console.log(h1.getAttribute("pantalla"));
-h1.setAttribute("pantalla","patito");
-console.log(h1.getAttribute("pantalla"));
-*/
-
-h1.classList.add("rojo");
-h1.classList.remove("rojo");
-// h1.classList.toggle("rojo");
-// h1.classList.constains("rojo"); //devuelve true o false
-
-input.value = "123456";
-
-//console.log(document.createElement("img"));
-const img = document.createElement("img");
-img.setAttribute("src",'https://i.pinimg.com/originals/8b/02/40/8b02409f975c98c89e15bc089e3bd289.gif');
-console.log(img);
-pid.appendChild(img);
+function bntOnClick() {
+    console.log(Number(input1.value) + Number(input2.value));
+    const sumaInput = Number(input1.value) + Number(input2.value);
+    result.innerHTML = "El resultado es:"+ sumaInput;
+}
