02c735a07e877ed9cd77e2be5eefb1590f254bfe
Claudia Villarroel <claudia.villarroel@live.com>
2024-01-06 18:30:01 -0300

    :hammer: addEventListener

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
-const h1 = document.querySelector("h1");
-const input1 = document.querySelector("#calculo1");
-const input2 = document.querySelector("#calculo2");
-const btn = document.querySelector("#btnCalcular");
-let result = document.querySelector("#result");
+const h1 = document.querySelector('h1');
+const form = document.querySelector('#form');
+const input1 = document.querySelector('#calculo1');
+const input2 = document.querySelector('#calculo2');
+const btn = document.querySelector('#btnCalcular');
+let result = document.querySelector('#result');
 
-function bntOnClick() {
-    console.log(Number(input1.value) + Number(input2.value));
-    const sumaInput = Number(input1.value) + Number(input2.value);
-    result.innerHTML = "El resultado es:"+ sumaInput;
+form.addEventListener('submit', sumarInputValues);
+
+/*
+btn.addEventListener("click", bntOnClick);
+*/
+function sumarInputValues(event) {
+  //console.log({event});
+  event.preventDefault();
+  console.log(Number(input1.value) + Number(input2.value));
+  const sumaInput = Number(input1.value) + Number(input2.value);
+  result.innerHTML = 'El resultado es:' + sumaInput;
 }
